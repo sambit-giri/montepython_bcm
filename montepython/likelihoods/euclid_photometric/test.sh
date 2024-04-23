61456f5ff0b19475c324946761920625665012cb
lesgourg <lesgourg@physik.rwth-aachen.de>
2024-04-23 15:08:17 +0200

    minor improvements in test scripted, renamed fiducial files

--- montepython/likelihoods/euclid_photometric/test.sh
+++ montepython/likelihoods/euclid_photometric/test.sh
@@ -1,10 +1,12 @@
 #! /bin/bash
 
+### This script shows how to run the euclid_photometric likelihood alone,
+### with the provided input file and covariance matrix
 ### USAGE:  launch this script from the montepython main dir
 
 INPUT=input/euclid_photometric_w0waMN.param
 CHAINS=chains/euclid_photometric
-Covmat=covmat/euclid_photometric_w0waMN.covmat
+COVMAT=covmat/euclid_photometric_w0waMN.covmat
 
 echo "delete chains folder"
 rm -rv $CHAINS
@@ -13,12 +15,14 @@ rm -rv ${CHAINS}_test
 echo "running superpessimistic case"
 cp -v montepython/likelihoods/euclid_photometric/euclid_photometric.data.pessimistic montepython/likelihoods/euclid_photometric/euclid_photometric.data
 
-echo "Creating fiducial"
-rm data/euclid_xc_fiducial.npz
+echo "Remove old fiducial file"
+rm data/euclid_photometric_fiducial.npz
+
+echo "Creating new fiducial file"
 python montepython/MontePython.py run -p $INPUT -o $CHAINS -f 0 -N 1
+
 echo "Testing chi-squared"
 python montepython/MontePython.py run -p $INPUT -o ${CHAINS}_test -f 0 -N 1 --display-each-chi2
 
 echo "Running chains"
-python montepython/MontePython.py run -o $CHAINS -f 1.9 -N 100000 --update 100 --superupdate 20 -c $Covmat
-
+python montepython/MontePython.py run -o $CHAINS -f 1.9 -N 100000 --update 100 --superupdate 20 -c $COVMAT
--- montepython/likelihoods/euclid_spectroscopic/test.sh
+++ montepython/likelihoods/euclid_spectroscopic/test.sh
@@ -1,10 +1,12 @@
 #! /bin/bash
 
+### This script shows how to run the euclid_spectroscopic likelihood alone,
+### with the provided input file and covariance matrix
 ### USAGE:  launch this script from the montepython main dir
 
 INPUT=input/euclid_spectropscopic_w0waMN.param
 CHAINS=chains/euclid_spectroscopic/
-Covmat=covmat/euclid_spectroscopic_w0waMN.covmat
+COVMAT=covmat/euclid_spectroscopic_w0waMN.covmat
 
 echo "delete chains folder"
 rm -rv $CHAINS
@@ -12,11 +14,14 @@ rm -rv $CHAINS
 echo "running superpessimistic case"
 cp -v montepython/likelihoods/euclid_spectroscopic/euclid_spectroscopic.data.superpessimistic montepython/likelihoods/euclid_spectroscopic/euclid_spectroscopic.data
 
-echo "Creating fiducial"
+echo "Remove old fiducial file"
+rm data/euclid_spectroscopic_fiducial.dat
+
+echo "Creating new fiducial file"
 python montepython/MontePython.py run -p $INPUT -o $CHAINS -f 0 -N 1
+
 echo "Testing chi-squared"
-python montepython/MontePython.py run -p $INPUT -o $CHAINS -f 0 -N 1 --display-each-chi2
+python montepython/MontePython.py run -p $INPUT -o ${CHAINS}_test -f 0 -N 1 --display-each-chi2
 
 echo "Running chains"
-python montepython/MontePython.py run -o $CHAINS -f 1.9 -N 100000 --update 100 --superupdate 20 -c $Covmat
-
+python montepython/MontePython.py run -o $CHAINS -f 1.9 -N 100000 --update 100 --superupdate 20 -c $COVMAT
